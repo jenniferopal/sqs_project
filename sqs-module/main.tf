8576a81205d38895442a806ad9d25a3cc208e5db
Jennifer Opal <contact@jenniferopal.co>
2025-07-09 15:25:31 +0100

    adding two iam policies to send, receive and delete sqs messages

--- sqs-module/main.tf
+++ sqs-module/main.tf
@@ -33,3 +33,40 @@ resource "aws_sqs_queue" "main_queue" {
         Name = each.key
     }
 }
+
+# creating IAM policies to consume and write to the sqs queues
+resource "aws_iam_policy" "sqs_consumer_policy" {
+    for_each = toset(var.queue_names)
+
+    name        = "${each.key}-consumer-policy"
+    description = "Policy to allow consuming messages from ${each.key} SQS queue"
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect   = "Allow"
+                Action   = ["sqs:ReceiveMessage", "sqs:DeleteMessage"]
+                Resource = aws_sqs_queue.main_queue[each.key].arn
+            }
+        ]
+    })
+}
+
+resource "aws_iam_policy" "sqs_producer_policy" {
+    for_each = toset(var.queue_names)
+
+    name        = "${each.key}-producer-policy"
+    description = "Policy to allow sending messages to ${each.key} SQS queue"
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect   = "Allow"
+                Action   = ["sqs:SendMessage"]
+                Resource = aws_sqs_queue.main_queue[each.key].arn
+            }
+        ]
+    })
+}
