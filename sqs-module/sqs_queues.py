98fb91e3244c891d13821045bb1cff5aa75c1183
Jennifer Opal <contact@jenniferopal.co>
2025-07-10 13:14:24 +0100

    modifying the script to print out the message count and queue names

--- sqs-module/sqs_queues.py
+++ sqs-module/sqs_queues.py
@@ -1,3 +1,4 @@
+import sys
 import boto3
 
 def get_queues_message_totals(queues: list):
@@ -9,13 +10,14 @@ def get_queues_message_totals(queues: list):
     :param queues: List of SQS queue URLs.
     :return: Dictionary with queue URLs as keys and message counts as values.
     """
+
     sqs = boto3.client('sqs')
     totals = {}
 
     for queue_url in queues:
         try:
             response = sqs.get_queue_attributes(
-                QueueUrl=queue_url,
+                QueueUrl=queue_url.split('/')[-1],  # Ensure the URL is clean
                 AttributeNames=['ApproximateNumberOfMessages']
             )
             totals[queue_url] = int(response['Attributes'].get('ApproximateNumberOfMessages', 0))
@@ -26,13 +28,12 @@ def get_queues_message_totals(queues: list):
     return totals
 
 if __name__ == "__main__":
-    # queue urls to check for message totals
+    # queue urls to check for message totals excluding dlqs
     queues = [
         "https://sqs.us-east-1.amazonaws.com/891612543788/green-queue",
-        "https://sqs.us-east-1.amazonaws.com/891612543788/green-queue-dlq",
-        "https://sqs.us-east-1.amazonaws.com/891612543788/red-queue",
-        "https://sqs.us-east-1.amazonaws.com/891612543788/red-queue-dlq"
+        "https://sqs.us-east-1.amazonaws.com/891612543788/red-queue"
     ]
+    queue_name = queues[0].split('/')[-1]  # Extract the queue name from the URL
     # get message totals for the queues
     totals = get_queues_message_totals(queues)
     print(totals)
